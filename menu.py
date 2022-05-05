b86a023fb8ee2d2604fc23f2ad1170a2efb8a05f
xxdmjj <65935269+xxdmjj@users.noreply.github.com>
2022-05-05 10:25:11 -0500

    Add files via upload
    It keeps pulling the false that is labeled first, what can I use for the program to actually read the input?

---
+++ menu.py
@@ -0,0 +1,26 @@
+
+drinks = { ('Coke'), ('Sprite'), ('Lemonade') }
+food = { ('Burger'), ('enchillada'), ('Rice') }
+
+drinks  = False
+food = True
+
+age = int(input("whats your age 21+"))
+
+if age > 20:
+    print("welcome to our app!")
+
+input("would you like food or a drink?")
+
+if(True):
+    {
+    print(drinks)
+    }
+
+if(False):
+    {
+    print(food)
+    }
+
+
+
